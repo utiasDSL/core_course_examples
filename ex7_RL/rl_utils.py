a4a527f9151980c30b7f61941a153ec6aa237e06
lukasbrunke <lukas.brunke@tu-harburg.de>
2025-11-26 20:06:13 +0100

    refactored mb-rl

--- ex7_RL/rl_utils.py
+++ ex7_RL/rl_utils.py
@@ -8,6 +8,7 @@ import time
 import os
 from scipy.integrate import solve_ivp
 from scipy.optimize import minimize
+import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 from abc import ABC, abstractmethod
@@ -29,7 +30,66 @@ from utils.controller import BaseController, check_input_constraints, timing
 
 
 
+def define_shifted_colormap():
+    def shiftedColorMap(cmap, start=0, midpoint=0.5, stop=1.0, name='shiftedcmap'):
+        '''
+        Function to offset the "center" of a colormap. Useful for
+        data with a negative min and positive max and you want the
+        middle of the colormap's dynamic range to be at zero.
 
+        From: https://stackoverflow.com/questions/7404116/defining-the-midpoint-of-a-colormap-in-matplotlib
+
+        Input
+        -----
+        cmap : The matplotlib colormap to be altered
+        start : Offset from lowest point in the colormap's range.
+            Defaults to 0.0 (no lower offset). Should be between
+            0.0 and `midpoint`.
+        midpoint : The new center of the colormap. Defaults to 
+            0.5 (no shift). Should be between 0.0 and 1.0. In
+            general, this should be  1 - vmax / (vmax + abs(vmin))
+            For example if your data range from -15.0 to +5.0 and
+            you want the center of the colormap at 0.0, `midpoint`
+            should be set to  1 - 5/(5 + 15)) or 0.75
+        stop : Offset from highest point in the colormap's range.
+            Defaults to 1.0 (no upper offset). Should be between
+            `midpoint` and 1.0.
+        '''
+        cdict = {
+            'red': [],
+            'green': [],
+            'blue': [],
+            'alpha': []
+        }
+
+        # regular index to compute the colors
+        reg_index = np.linspace(start, stop, 257)
+
+        # shifted index to match the data
+        shift_index = np.hstack([
+            np.linspace(0.0, midpoint, 128, endpoint=False), 
+            np.linspace(midpoint, 1.0, 129, endpoint=True)
+        ])
+
+        for ri, si in zip(reg_index, shift_index):
+            r, g, b, a = cmap(ri)
+
+            cdict['red'].append((si, r, r))
+            cdict['green'].append((si, g, g))
+            cdict['blue'].append((si, b, b))
+            cdict['alpha'].append((si, a, a))
+
+        newcmap = matplotlib.colors.LinearSegmentedColormap(name, cdict)
+        # plt.register_cmap(cmap=newcmap)
+        matplotlib.colormaps.register(name=name, cmap=newcmap)
+
+        return newcmap
+
+    orig_cmap = matplotlib.cm.coolwarm
+    midpoint = 1 - 10 / (10 + 1)
+
+    shifted_cmap = shiftedColorMap(orig_cmap, midpoint=midpoint, name='shifted')
+    return shifted_cmap
 
 
 '''------Model-based RL Controller------'''
